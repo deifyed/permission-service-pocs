bf4f576599de8026a103b5e6601f1ecf8a057496
Julius Pedersen <julius.pedersen@origo.oslo.kommune.no>
2025-07-30 14:55:10 +0200

    ✅ Add admin relation

--- test.sh
+++ test.sh
@@ -13,11 +13,19 @@ keto() {
 
 set -f
 
-echo "keto check "*" view Schema 50; expect Denied"
+echo "keto check \"*\" view Schema 50; expect Denied"
 keto check "*" view Schema 50
-echo "keto check "*" edit Schema 50; expect Denied"
+
+echo "keto check \"*\" edit Schema 50; expect Denied"
 keto check "*" edit Schema 50
-echo "keto check "julius" view Schema 50; expect Allowed"
+
+echo "keto check \"julius\" view Schema 50; expect Allowed"
 keto check "julius" view Schema 50
-echo "keto check "julius" edit Schema 50; expect Denied"
+
+echo "keto check \"julius\" edit Schema 50; expect Denied"
 keto check "julius" edit Schema 50
+
+echo "keto check \"sjefen\" view Schema 50; expect Allowed"
+keto check "sjefen" view Schema 50
+echo "keto check \"sjefen\" edit Schema 50; expect Allowed"
+keto check "sjefen" edit Schema 50
